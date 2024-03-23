65a24bc2dafe1e4f543c83d46c6588414d46fd41
Ga1lahad <brunodevoficial@gmail.com>
2024-03-22 21:03:17 -0300

    Criação da primeira tela arquivos e css padrão

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="utilitario/css/style.css">
+    <title>Menu Inicia</title>
+</head>
+
+<body>
+    <nav>
+        <div>
+            <a href="">Logo</a>
+        </div>
+    </nav>
+</body>
+
+</html>
